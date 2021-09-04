98c1d87c81548ecb904073b02268b249a37a7edf
Wer3t <yaylo.danil@gmail.com>
2021-09-04 14:51:54 +0300

    save into db

--- app/src/main/java/com/example/studnotes/MainActivity.kt
+++ app/src/main/java/com/example/studnotes/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.studnotes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.studnotes.db.MyDbManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val dbManager = MyDbManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,4 +17,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dbManager.closeDb()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dbManager.openDb()
+    }
+
 }
--- app/src/main/java/com/example/studnotes/NotesEditor.kt
+++ app/src/main/java/com/example/studnotes/NotesEditor.kt
@@ -1,21 +1,73 @@
 package com.example.studnotes
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Toast
+import com.example.studnotes.db.MyDbManager
 import kotlinx.android.synthetic.main.activity_notes_editor.*
 
 class NotesEditor : AppCompatActivity() {
+    val imageRequestCode = 10
+    var tempImgUri = "empty"
+    val dbManager = MyDbManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes_editor)
-        fabAddPicture.setOnClickListener { imViewLayout.visibility = View.VISIBLE }
+
+        fabAddPicture.setOnClickListener {
+            imViewLayout.visibility = View.VISIBLE
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, imageRequestCode)
+            fabAddPicture.visibility = View.GONE
+        }
+
         ibDeleteImg.setOnClickListener{
             val anim : Animation= AnimationUtils.loadAnimation(this, R.anim.click)
             ibDeleteImg.startAnimation(anim)
             imViewLayout.visibility = View.GONE
+            fabAddPicture.visibility = View.VISIBLE
+            tempImgUri = "empty"
+        }
+
+        ibEditImg.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, imageRequestCode)
+        }
+
+        fabSaveNote.setOnClickListener {
+            val subject = etSubject.text.toString()
+            val title = etTitle.text.toString()
+            val note = etNote.text.toString()
+            if(subject != "" && title != "" && note != ""){
+                dbManager.insertToDb(subject, title, note, tempImgUri)
+                Toast.makeText(this, "Заметка сохранена!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dbManager.closeDb()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dbManager.openDb()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == imageRequestCode){
+            ivMainImage.setImageURI(data?.data)
+            tempImgUri = data?.data.toString()
         }
     }
 }
